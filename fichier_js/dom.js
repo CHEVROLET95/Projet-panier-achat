b0cbfdc3e9e49b5794ce1b76e99eb2a08cd1e703
CHEVROLET95 <moisezoucou@gmail.com>
2024-06-14 12:57:10 +0200

    projet en cour

---
+++ fichier_js/dom.js
@@ -0,0 +1,50 @@
+console.log("hello word")
+
+
+
+
+const button_plus = document.querySelector('.button_plus')
+
+
+console.log(button_plus)
+
+
+
+
+button_plus.addEventListener('click', function(){
+    const quantite_number = document.querySelector('.quantite_number').textContent
+
+    const new_quantite= parseInt(quantite_number,10)+1
+
+
+    document.querySelector('.quantite_number').textContent = new_quantite
+})
+
+
+
+
+const button_moyen = document.querySelector('.button_moyen')
+
+console.log(button_moyen)
+
+button_moyen.addEventListener('click', function(){
+    const quantite_number= document.querySelector('.quantite_number').textContent
+
+    const baisse_quantite= parseInt(quantite_number, 10)-1
+
+    document.querySelector('.quantite_number').textContent = baisse_quantite
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
